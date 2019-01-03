eb1bade4633a3e359637aa39e0aaaffcc13de6c0
mauritscottyn <34495991+mauritscottyn@users.noreply.github.com>
2019-01-03 14:49:49 +0100

    added operator overloading to eq and lt + str method to class Locatie

--- thuisproject2/models/Locatie.py
+++ thuisproject2/models/Locatie.py
@@ -39,4 +39,22 @@ class Locatie:
             raise ValueError('De instantie longitude mag enkel een float zijn.')
 
 
+    def __str__(self):
+        return f'Straat: {self.straat}  Latitude: {self.latitude}   Longitude: {self.longitude}'
+
+
+    def __lt___(self, other):
+        if self.straat.lower() < other.straat.lower():
+            return True
+        else:
+            return False
+
+
+    def __eq__ (self, other):
+        if self.straat.lower() == other.straat.lower() and self.latitude == other.latitude and self.longitude == other.longitude:
+            return True
+        else:
+            return False
+
+
 
