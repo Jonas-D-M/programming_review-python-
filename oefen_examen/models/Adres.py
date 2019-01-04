8e8272597fe56169328bb356e7d6eee3f480f145
mauritscottyn <34495991+mauritscottyn@users.noreply.github.com>
2019-01-04 18:31:52 +0100

    added eq and lt methods to class adres

--- oefen_examen/models/Adres.py
+++ oefen_examen/models/Adres.py
@@ -61,3 +61,17 @@ class Adres():
 
     def __str__(self):
         return f'Straat: {self.straat}  Huisnummer: {self.huisnummer}   Postcode: {self.postcode}   Gemeente: {self.gemeente}'
+
+
+    def __eq__(self, other):
+        if self.straat.lower() == other.straat.lower and self.huisnummer == other.huisnummer and self.postcode == other.postcode and self.gemeente.lower() == other.gemeente.lower():
+            return True
+        else:
+            return False
+
+
+    def __lt__(self, other):
+        if self.straat.lower() < other.straat.lower():
+            return True
+        else:
+            return False
